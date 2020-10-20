087bf0df20e230c8d33d06e5eb1c3d3826a28067
GMurnevaite <greta.murnevaite@gmail.com>
2020-10-20 16:56:15 +0300

    added Real Estate class

---
+++ MERG_PSI/MERG_PSI/RealEstate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MERG_PSI
+{
+    class RealEstate
+    {
+        public string Link { get; set; }
+        public double Size { get; set; }
+        public double PricePerSquare { get; set; }
+        public int Rooms { get; set; }
+        public string Floor { get; set; }
+        public string Construction { get; set; }
+        public string Heating { get; set; }
+        public int BuildYear { get; set; }
+        public string MapLink { get; set; }
+        public string City { get; set; }
+        public string Municipality { get; set; }
+        public string Street { get; set; }
+        public string HouseNm { get; set; }
+    }
+}
