b6790b62f702befcf1b19c57ed0e17878edc8dd0
GMurnevaite <greta.murnevaite@gmail.com>
2020-10-23 14:55:59 +0300

    added DeserializationFromJson class

---
+++ MERG_PSI/MERG_PSI/DeserializationFromJson.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MERG_PSI
+{
+    
+    class DeserializationFromJson
+    {
+        public List<RealEstate> Data { get; set; }
+        public DeserializationFromJson()
+        {
+            var filePath = "../../scrapedData.txt";
+
+            string jsonFromFIle;
+            using (var reader = new StreamReader(filePath))
+            {
+                jsonFromFIle = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            Data = JsonConvert.DeserializeObject<List<RealEstate>>(jsonFromFIle);
+             
+        }
+    }
+}
