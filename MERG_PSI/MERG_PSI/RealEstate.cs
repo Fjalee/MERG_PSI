453e68a0dd18dd54d7ece75e326aa8fc8c4c9f2f
Fjalee <cepRytis@gmail.com>
2020-10-30 00:46:47 +0200

    RealEstate class

--- MERG_PSI/MERG_PSI/RealEstate.cs
+++ MERG_PSI/MERG_PSI/RealEstate.cs
@@ -2,14 +2,14 @@
 
 namespace MERG_PSI
 {
-    class RealEstate
+    public class RealEstate
     {
         public string Link { get; set; }
         public double Area { get; set; }
         public double PricePerSqM { get; set; }
         public int NumberOfRooms { get; set; }
         public string Floor { get; set; }
-        private double _calculatedPrice { get; set; }
+        private readonly double _calculatedPrice;
         public double ScrapedPrice { get; set; }
         public double Price { get; set; }
         public string MapLink { get; set; }
