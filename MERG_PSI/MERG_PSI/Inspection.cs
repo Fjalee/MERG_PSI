da7ef7b95ab536834010934a1e51f3fb25628a3b
Fjalee <cepRytis@gmail.com>
2020-10-30 15:42:50 +0200

    Inspection class

--- MERG_PSI/MERG_PSI/Inspection.cs
+++ MERG_PSI/MERG_PSI/Inspection.cs
@@ -18,20 +18,34 @@ namespace MERG_PSI
             var filters = new Filters();
             var filteredList = listOfRealEstate;
 
-            filteredList = IsFilterValueSelected(filtersValue[0], filtersValue[1]) ? filters.FilterRealEstateByPrice(filteredList, priceFrom: int.Parse(filtersValue[0]), priceTo: int.Parse(filtersValue[1])) : filteredList;
+            filteredList = IsFilterValueSelected(filtersValue[0], filtersValue[1])
+                ? filters.FilterRealEstateByPrice(filteredList, priceFrom: int.Parse(filtersValue[0]), priceTo: int.Parse(filtersValue[1]))
+                : filteredList;
 
-            filteredList = IsFilterValueSelected(filtersValue[2], filtersValue[3]) ? filters.FilterRealEstateByArea(filteredList, areaFrom: int.Parse(filtersValue[2]), areaTo: int.Parse(filtersValue[3])) : filteredList;
+            filteredList = IsFilterValueSelected(filtersValue[2], filtersValue[3])
+                ? filters.FilterRealEstateByArea(filteredList, areaFrom: int.Parse(filtersValue[2]), areaTo: int.Parse(filtersValue[3]))
+                : filteredList;
 
-            filteredList = !string.IsNullOrEmpty(filtersValue[4]) ? filters.FilterRealEstateByMunicipality(filteredList, municipality: filtersValue[4]) : filteredList;
+            filteredList = !string.IsNullOrEmpty(filtersValue[4])
+                ? filters.FilterRealEstateByMunicipality(filteredList, municipality: filtersValue[4])
+                : filteredList;
 
-            filteredList = !string.IsNullOrEmpty(filtersValue[5]) ? filters.FilterRealEstateByStreet(filteredList, street: filtersValue[5]) : filteredList;
+            filteredList = !string.IsNullOrEmpty(filtersValue[5])
+                ? filters.FilterRealEstateByStreet(filteredList, street: filtersValue[5])
+                : filteredList;
 
-            filteredList = IsFilterValueSelected(filtersValue[6], filtersValue[7]) ? filters.FilterRealEstateByPricePerSqM(filteredList, pricePerSqMFrom: int.Parse(filtersValue[6]), pricePerSqMTo: int.Parse(filtersValue[7])) : filteredList;
+            filteredList = IsFilterValueSelected(filtersValue[6], filtersValue[7])
+                ? filters.FilterRealEstateByPricePerSqM(filteredList, pricePerSqMFrom: int.Parse(filtersValue[6]), pricePerSqMTo: int.Parse(filtersValue[7]))
+                : filteredList;
 
-            filteredList = IsFilterValueSelected(filtersValue[8], filtersValue[9]) ? filters.FilterRealEstateByBuildYear(filteredList, buildYearFrom: int.Parse(filtersValue[8]), buildYearTo: int.Parse(filtersValue[9]), noInfoBuildYear) : noInfoBuildYear ? filters.FilterByRealEstateWhenNoInfoBuildYear(filteredList) : filteredList;
+            filteredList = IsFilterValueSelected(filtersValue[8], filtersValue[9])
+                ? filters.FilterRealEstateByBuildYear(filteredList, buildYearFrom: int.Parse(filtersValue[8]), buildYearTo: int.Parse(filtersValue[9]), noInfoBuildYear)
+                : noInfoBuildYear ? filters.FilterByRealEstateWhenNoInfoBuildYear(filteredList) : filteredList;
 
-            filteredList = IsFilterValueSelected(filtersValue[10], filtersValue[11]) ? filters.FilterRealEstateByNumberOfRooms(filteredList, numberOfRoomsFrom: int.Parse(filtersValue[10]), numberOfRoomsTo: int.Parse(filtersValue[11]), noInfoRoomsNumber) : noInfoRoomsNumber ? filters.FilterByRealEstateWhenNoInfoNumberOfRooms(filteredList) : filteredList;
-    
+            filteredList = IsFilterValueSelected(filtersValue[10], filtersValue[11])
+                ? filters.FilterRealEstateByNumberOfRooms(filteredList, numberOfRoomsFrom: int.Parse(filtersValue[10]), numberOfRoomsTo: int.Parse(filtersValue[11]), noInfoRoomsNumber)
+                : noInfoRoomsNumber ? filters.FilterByRealEstateWhenNoInfoNumberOfRooms(filteredList) : filteredList;
+
             return filteredList;
         }
     }
