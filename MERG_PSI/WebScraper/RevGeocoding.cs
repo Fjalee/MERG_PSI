ff12f0b95b1d0cd80b96266258570073ba4db816
Greta Murnevaitė <greta.murnevaite@gmail.com>
2020-11-15 12:54:39 +0200

    output text with lithuanian characters

--- MERG_PSI/WebScraper/RevGeocoding.cs
+++ MERG_PSI/WebScraper/RevGeocoding.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using AngleSharp.Text;
+using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Net;
+using System.Text;
 
 namespace WebScraper
 {
@@ -8,11 +11,13 @@ namespace WebScraper
     {
         private const string _apiKey = "AIzaSyBNcsDmpKTd-qo5XLSkgFZcd1WYh_SvOiw";
         private readonly string _baseUrlRGC = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
-        private readonly string _plusUrl = "&key=" + _apiKey + "&sensor=false" + "&language=lt";//&region=LT";
+        private readonly string _plusUrl = "&key=" + _apiKey + "&sensor=false";// + "&language=lt";//&region=LT";
 
         public string CoordsToAdress(double latitude, double longitude, string name)
         {
-            var json = new WebClient().DownloadString(_baseUrlRGC + latitude + ","
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var json = wc.DownloadString(_baseUrlRGC + latitude + ","
                 + longitude + _plusUrl);
             var jsonResult = JsonConvert.DeserializeObject<GoogleGeoCodeResponse>(json);
 
@@ -25,7 +30,7 @@ namespace WebScraper
                 {
                     foreach (var j in i.Address_components)
                     {
-                        foreach (var k in j.Types)
+                        foreach(var k in j.Types)
                         {
                             if (k == name)
                             {
@@ -33,7 +38,7 @@ namespace WebScraper
                             }
                         }
                     }
-                }  
+                }
                 return String.Empty;  
             }
             else
