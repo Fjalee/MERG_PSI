3538f9479b48473e810ea0a376c762c47ff46e6a
Greta Murnevaitė <greta.murnevaite@gmail.com>
2020-11-15 11:19:38 +0200

    new class that get adress from coordinates

---
+++ MERG_PSI/WebScraper/RevGeocoding.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace WebScraper
+{
+    class RevGeocoding
+    {
+        private const string _apiKey = "AIzaSyBNcsDmpKTd-qo5XLSkgFZcd1WYh_SvOiw";
+        private readonly string _baseUrlRGC = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+        private readonly string _plusUrl = "&key=" + _apiKey + "&sensor=false" + "&language=lt";//&region=LT";
+
+        public string CoordsToAdress(double latitude, double longitude, string name)
+        {
+            var json = new WebClient().DownloadString(_baseUrlRGC + latitude + ","
+                + longitude + _plusUrl);
+            var jsonResult = JsonConvert.DeserializeObject<GoogleGeoCodeResponse>(json);
+
+            var status = jsonResult.Status;
+            var geoLocation = String.Empty;
+
+            if (status == "OK")
+            {
+                foreach (var i in jsonResult.Results)
+                {
+                    foreach (var j in i.Address_components)
+                    {
+                        foreach (var k in j.Types)
+                        {
+                            if (k == name)
+                            {
+                                return j.Long_name;
+                            }
+                        }
+                    }
+                }  
+                return String.Empty;  
+            }
+            else
+            {
+                return status;
+            }
+
+        }
+    }
+}
