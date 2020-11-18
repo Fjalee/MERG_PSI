dbc3a9985c3097a5ef80c75f0a3bdf6fd6418b5e
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2020-11-18 22:43:40 +0200

    adding all pin points when you click on map tabbar

--- App/App/App/Views/MapPage.xaml.cs
+++ App/App/App/Views/MapPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using App.Models;
+using App.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
@@ -8,14 +9,27 @@ namespace App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        RealEstate Mock => DependencyService.Get<RealEstate>();
         readonly RealEstateModel MyValue = new RealEstateModel();
         public MapPage ()
         {
             InitializeComponent();
-            
+
+            var realEstates = GetData().Result;
             var position = new Position(55.1694, 23.8813);
             var mapSpan = new MapSpan(position, 4, 4);
             var map = new Map(mapSpan);
+            foreach (var house in realEstates)
+            {
+                var pin = new Pin
+                {
+                    Position = new Position(house.Latitude, house.Longitude),
+                    Label = house.Municipality,
+                    Address = $"Kaina: { house.Price} € Kaina / m²: { house.PricePerSqM} €/ m² Plotas: { house.Area}m² Metai: { house.BuildYear} Kambariai: { house.NumberOfRooms} Savivaldybė: { house.Municipality} Mikrorajonas: { house.Microdistrict} Gatvė: { house.Street}"
+
+                };
+                map.Pins.Add(pin);
+            }
             container.Children.Add(map);
         }
         public MapPage(RealEstateModel realEstate)
@@ -35,6 +49,13 @@ namespace App.Views
         };
             map.Pins.Add(pin);
             container.Children.Add(map);
+        }
+        public async System.Threading.Tasks.Task<System.Collections.Generic.List<RealEstateModel>> GetData()
+        {
+
+            return await Mock.GetRealEstates();
+
+           // myItem.ItemsSource = newItem;
         }
     }
 }
