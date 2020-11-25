f2b2430fbf1815336c553861ba6c1613ccb17882
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2020-11-25 21:26:05 +0200

    adding list of RealEstates in HomePage

--- MERG_PSI/Xamarin_UI/Xamarin_UI/Views/HomePage.xaml.cs
+++ MERG_PSI/Xamarin_UI/Xamarin_UI/Views/HomePage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using MERG_BackEnd;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
-using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -20,8 +16,13 @@ namespace Xamarin_UI.Views
             var assembly = typeof(HomePage).GetTypeInfo().Assembly;
             var stream = assembly.GetManifestResourceStream("Xamarin_UI.Resources.scrapedData.txt");
             var listOfRealEstates = new Data(stream).SampleData;
+            Populate(listOfRealEstates);
 
+        }
 
+        public void Populate (List<RealEstate> listOfRealEstates)
+        {
+            myItem.ItemsSource = listOfRealEstates;
         }
 
     }
