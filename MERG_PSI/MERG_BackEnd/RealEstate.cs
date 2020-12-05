0644f4b70007d5b26c32b31fbc7ad08ff15502f0
Rytis Cepulis <69936179+Fjalee@users.noreply.github.com>
2020-12-05 16:02:32 +0200

    Update RealEstate.cs

--- MERG_PSI/MERG_BackEnd/RealEstate.cs
+++ MERG_PSI/MERG_BackEnd/RealEstate.cs
@@ -4,8 +4,7 @@ namespace MERG_BackEnd
 {
     public class RealEstate : RealEstateModel
     {
-        override
-        public string ToString()
+        override public string ToString()
         {
             return $"\nKaina: {Price} €\nKaina/m²: {PricePerSqM} €/m²\nPlotas: {Area} m²\nMetai: {BuildYear}\nKambariai: {NumberOfRooms}\nSavivaldybė: {Municipality}\nMikrorajonas: {Microdistrict}\nGatvė: {Street}";
         }
