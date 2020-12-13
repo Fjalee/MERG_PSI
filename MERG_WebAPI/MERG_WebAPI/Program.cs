963001a8e41bd3a7c27ea59070809c7839cd29c3
Greta Murnevaitė <greta.murnevaite@gmail.com>
2020-12-13 02:29:20 +0200

    add config

--- MERG_WebAPI/MERG_WebAPI/Program.cs
+++ MERG_WebAPI/MERG_WebAPI/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace MERG_WebAPI
 {
@@ -13,7 +15,32 @@ namespace MERG_WebAPI
     {
         public static void Main(string[] args)
         {
+             try
+            {
+                var con = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString);
+                var str = "select * from realestate";
+                var command = new MySqlCommand(str, con);
+                con.Open();
+                using (var r = command.ExecuteReader())
+                {
+                    while(r.Read())
+                    {
+                        var first = r.GetString(0);
+                        var second = r.GetValue(1);
+
+                        Console.WriteLine($"{first}  {second}");
+
+                    }
+                }
+                Console.WriteLine("Pavyko");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message + "Nepavyko :(");
+            }
             CreateHostBuilder(args).Build().Run();
+
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
