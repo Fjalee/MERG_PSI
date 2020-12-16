4f2d866ff7116b7bf38200dd9dd2a9cb0ae5afa8
Greta Murnevaitė <greta.murnevaite@gmail.com>
2020-12-16 23:16:36 +0200

    Migrations

---
+++ MERG_WebAPI/Database/AppDbcontext.cs
@@ -0,0 +1,18 @@
+﻿using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<RealEstate> RealEstates { get; set; }
+        public DbSet<Municipality> Municipalities { get; set; }
+        public DbSet<Microdistrict> Microdistricts { get; set; }
+        public DbSet<Street> Streets { get; set; }
+    }
+}
---
+++ MERG_WebAPI/Database/Migrations/20201216200049_Init.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Microdistricts",
+                columns: table => new
+                {
+                    MicrodistrictId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Microdistricts", x => x.MicrodistrictId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Municipalities",
+                columns: table => new
+                {
+                    MunicipalityId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Municipalities", x => x.MunicipalityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstates",
+                columns: table => new
+                {
+                    RealEstateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Link = table.Column<string>(nullable: true),
+                    Area = table.Column<double>(nullable: false),
+                    PricePerSqM = table.Column<double>(nullable: false),
+                    NumberOfRooms = table.Column<int>(nullable: false),
+                    Floor = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    MapLink = table.Column<string>(nullable: true),
+                    Municipality = table.Column<string>(nullable: true),
+                    Microdistrict = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    BuildYear = table.Column<int>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstates", x => x.RealEstateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Streets",
+                columns: table => new
+                {
+                    StreetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Streets", x => x.StreetId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Microdistricts");
+
+            migrationBuilder.DropTable(
+                name: "Municipalities");
+
+            migrationBuilder.DropTable(
+                name: "RealEstates");
+
+            migrationBuilder.DropTable(
+                name: "Streets");
+        }
+    }
+}
