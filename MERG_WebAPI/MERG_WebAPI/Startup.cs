9e08d433298b0faf124cb27b25ed864689dae7a6
Greta Murnevaitė <greta.murnevaite@gmail.com>
2020-12-16 23:14:41 +0200

    added connection string

--- MERG_WebAPI/MERG_WebAPI/Startup.cs
+++ MERG_WebAPI/MERG_WebAPI/Startup.cs
@@ -1,15 +1,10 @@
+using Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MERG_WebAPI
 {
@@ -27,6 +22,7 @@ namespace MERG_WebAPI
         {
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddDbContext<AppDbcontext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
