a2ff661a526304ea9fcf76550ab4a001f3a4acb4
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2020-12-18 12:44:04 +0200

    adding municipality controller

---
+++ MERG_WebAPI/MERG_BackEnd/IFiltersValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MERG_BackEnd
+{
+    public interface IFiltersValue
+    {
+        public string Municipality { get; set; }
+        public string Microdistrict { get; set; }
+        public string Street { get; set; }
+        public Tuple<bool, int> PriceFrom { get; set; }
+        public Tuple<bool, int> PriceTo { get; set; }
+        public Tuple<bool, int> AreaTo { get; set; }
+        public Tuple<bool, int> AreaFrom { get; set; }
+        public Tuple<bool, int> BuildYearFrom { get; set; }
+        public Tuple<bool, int> BuildYearTo { get; set; }
+        public Tuple<bool, int> NumberOfRoomsFrom { get; set; }
+        public Tuple<bool, int> NumberOfRoomsTo { get; set; }
+        public Tuple<bool, int> PricePerSqMFrom { get; set; }
+        public Tuple<bool, int> PricePerSqMTo { get; set; }
+        public bool NoBuildYearInfo { get; set; }
+        public bool NoNumberOfRoomsInfo { get; set; }
+    }
+}
---
+++ MERG_WebAPI/MERG_WebAPI/Controllers/MunicipalityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MERG_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunicipalityController : ControllerBase
+    {
+
+    }
+}
