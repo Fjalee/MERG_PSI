c65af8b2509e13bf67a53637015d70f39d4472bf
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2020-12-18 13:18:41 +0200

    get method which returns municipality list

--- MERG_WebAPI/MERG_WebAPI/Controllers/MunicipalityController.cs
+++ MERG_WebAPI/MERG_WebAPI/Controllers/MunicipalityController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database;
+using Database.Entities;
+using MERG_BackEnd;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MERG_WebAPI.Controllers
 {
@@ -6,6 +11,19 @@ namespace MERG_WebAPI.Controllers
     [ApiController]
     public class MunicipalityController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public MunicipalityController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Municipality> Get()
+        {
+            var municipalities = _context.Municipalities.ToList();
+            return municipalities;
+        }
 
     }
 }
