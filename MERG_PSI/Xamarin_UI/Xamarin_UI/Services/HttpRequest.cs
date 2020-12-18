36e911dcd6f7da2fa95f34e3a4cac2d0ca07b82d
mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>
2020-12-18 22:22:29 +0200

    creating class for api requests

---
+++ MERG_PSI/Xamarin_UI/Xamarin_UI/Services/HttpRequest.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Xamarin_UI.Services
+{
+    public class HttpRequest
+    {
+        private readonly HttpClient _httpClient;
+        private const string _webApiLink = @"https://mergwebapi20201216191928.azurewebsites.net/";
+        private const string _municipalityUri = @"api/Municipality";
+        public HttpRequest()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<ObservableCollection<string>> GetMunicipalities()
+        {
+            var municipalities = new List<string>();
+            var uri = new Uri($"{_webApiLink}{_municipalityUri}");
+            try
+            {
+                var response = await _httpClient.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    if (content != null)
+                    {
+                        municipalities = JsonConvert.DeserializeObject<List<string>>(content);
+                        
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            var municipalityList = new ObservableCollection<string>(municipalities);
+            return municipalityList;
+        }
+    }
+}
