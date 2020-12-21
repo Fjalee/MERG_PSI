dc4f28d90ec5ebbe48ee825b0dd4e8f8e6e7e86f
Rytis <cepRytis@gmail.com>
2020-12-21 19:25:43 +0200

    RealEstateController get list of RealEstate from db and use it instead of previuosly used DummyDB Class

--- MERG_WebAPI/MERG_WebAPI/Controllers/RealEstateController.cs
+++ MERG_WebAPI/MERG_WebAPI/Controllers/RealEstateController.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using MERG_BackEnd;
+using Database;
+using Common;
 
 namespace MERG_WebAPI.Controllers
 {
@@ -14,17 +15,23 @@ namespace MERG_WebAPI.Controllers
         //dont delete comment below, its for testing
         //https://localhost:44376/api/RealEstate/SampleMunicipality3/SampleMicrodistrict3/SampleStreet3/false/0/false/0/false/0/false/0/false/0/false/0/false/0/false/0/false/0/false/0/false/false
 
-        private readonly List<RealEstate> _listOfRealEstates;   //fix, this should be deleted when data base is created
         private readonly IInspection _inspection;
+        private readonly AppDbContext _dbContext;
 
-        public RealEstateController(IInspection inspection)
+        public RealEstateController(IInspection inspection, AppDbContext dbContext)
         {
             _inspection = inspection;
-            _listOfRealEstates = new DummyDB().ListOfRealEstates;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            var _listOfRealEstates = _dbContext.RealEstates.Select(x => ((RealEstateModel)x)).ToList();
         }
 
         [HttpGet("{municipality}/{microdistrict}/{street}/{isPriceFrom}/{priceFrom}/{isPriceTo}/{priceTo}/{isAreaFrom}/{areaFrom}/{isAreaTo}/{areaTo}/{isBuildYearFrom}/{buildYearFrom}/{isBuildYearTo}/{buildYearTo}/{isNumberOfRoomsFrom}/{numberOfRoomsFrom}/{isNumberOfRoomsTo}/{numberOfRoomsTo}/{isPricePerSqMFrom}/{pricePerSqMFrom}/{isPricePerSqMTo}/{pricePerSqMTo}/{noInfoBuildYear}/{noInfoRoomNumber}")]
-        public List<RealEstate> Get(string municipality, string microdistrict, string street,
+        public List<RealEstateModel> Get(string municipality, string microdistrict, string street,
             bool isPriceFrom, int priceFrom, bool isPriceTo, int priceTo,
             bool isAreaFrom, int areaFrom, bool isAreaTo, int areaTo,
             bool isBuildYearFrom, int buildYearFrom, bool isBuildYearTo, int buildYearTo,
@@ -32,6 +39,7 @@ namespace MERG_WebAPI.Controllers
             bool isPricePerSqMFrom, int pricePerSqMFrom, bool isPricePerSqMTo, int pricePerSqMTo,
             bool noInfoBuildYear, bool noInfoRoomNumber)
         {
+            var _listOfRealEstates = _dbContext.RealEstates.Select(x => (RealEstateModel)x).ToList();
 
             var filter = new FiltersValue(municipality: municipality, microdistrict: microdistrict, street: street,
                priceFrom: new Tuple<bool, int>(isPriceFrom, priceFrom),
