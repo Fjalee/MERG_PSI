9644df783331a3adecbb370beb8a22dcf63f7747
GMurnevaite <71342819+GMurnevaite@users.noreply.github.com>
2021-01-01 14:44:44 +0200

    Custom pins (#171)
    * adding customMap and customPin
    
    * CustomMap and CustomPin
    
    * Custom map renderer, override some methods
    
    * map pin infowindow view
    
    * remove unnecessary parts of the code
    
    * Update CustomMapRenderer.cs
    
    update info window view in case if building year is equal to 0
    
    Co-authored-by: mickuseligijus <71340667+mickuseligijus@users.noreply.github.com>

--- MERG_PSI/Xamarin_UI/Xamarin_UI/Views/FullMapPage.xaml.cs
+++ MERG_PSI/Xamarin_UI/Xamarin_UI/Views/FullMapPage.xaml.cs
@@ -4,6 +4,7 @@ using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Xamarin_UI.Services;
 
 namespace Xamarin_UI.Views
 {
@@ -14,29 +15,40 @@ namespace Xamarin_UI.Views
         {
             InitializeComponent();
 
-            var position = new Position(55.1694, 23.8813);
-            var mapSpan = new MapSpan(position, 4, 4);
-            var map = new Xamarin.Forms.Maps.Map(mapSpan);
-
-            map = AddPinsToMap(realEstate, map);
-            container.Children.Add(map);
+            var customMap = new CustomMap
+            {
+                MapType = MapType.Street
+            };
+            
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(55.1694, 23.8813), Distance.FromMiles(110.0)));
+            customMap = AddPinsToMap(realEstate, customMap);
+            container.Children.Add(customMap);
         }
 
-        private Xamarin.Forms.Maps.Map AddPinsToMap(List<RealEstate> realEstate, Xamarin.Forms.Maps.Map map)
+        private CustomMap AddPinsToMap(List<RealEstate> realEstate, CustomMap map)
         {
+            map.CustomPins = new List<CustomPin> { };
             foreach (var house in realEstate)
             {
-                var pin = new Pin
+                var customPin = new CustomPin
                 {
+                    Type = PinType.Place,
                     Position = new Position(house.Latitude, house.Longitude),
                     Label = house.Municipality,
-                    Address = $"Kaina: { house.Price} € Kaina / m²: { house.PricePerSqM} €/ m² Plotas: { house.Area}m² Metai: { house.BuildYear} Kambariai: { house.NumberOfRooms} Savivaldybė: { house.Municipality} Mikrorajonas: { house.Microdistrict} Gatvė: { house.Street}"
+                    Address = house.Microdistrict + ", " + house.Street,
+                    PricePerSqM = house.PricePerSqM,
+                    NumberOfRooms = house.NumberOfRooms,
+                    BuildYear = house.BuildYear,
+                    Municipality = house.Municipality,
+                    Floor = house.Floor,
+                    Area = house.Area,
+                    Price = house.Price,
+                    Image = house.Image,
+                    Link = house.Link
                 };
-                pin.InfoWindowClicked += async (sender, e) =>
-                {
-                    await Browser.OpenAsync(house.Link, BrowserLaunchMode.SystemPreferred);
-                };
-                map.Pins.Add(pin);
+
+                map.CustomPins.Add(customPin);
+                map.Pins.Add(customPin);
             }
             return map;
         }
--- MERG_PSI/Xamarin_UI/Xamarin_UI/Views/MapPage.xaml.cs
+++ MERG_PSI/Xamarin_UI/Xamarin_UI/Views/MapPage.xaml.cs
@@ -1,36 +1,45 @@
 ﻿using MERG_BackEnd;
-using Xamarin.Essentials;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Xamarin_UI.Services;
 
 namespace Xamarin_UI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
-
         public MapPage(RealEstate realEstate)
         {
             InitializeComponent();
 
-            var position = new Position(realEstate.Latitude, realEstate.Longitude);
-            var mapSpan = new MapSpan(position, 0.1, 0.1);
-            var map = new Xamarin.Forms.Maps.Map(mapSpan);
-
-            var pin = new Pin
+            var customMap = new CustomMap
             {
+                MapType = MapType.Street
+            };
+
+            var customPin = new CustomPin
+            {
+                Type = PinType.Place,
                 Position = new Position(realEstate.Latitude, realEstate.Longitude),
                 Label = realEstate.Municipality,
-                Address = $"Kaina: { realEstate.Price} € Kaina / m²: { realEstate.PricePerSqM} €/ m² Plotas: { realEstate.Area}m² Metai: { realEstate.BuildYear} Kambariai: { realEstate.NumberOfRooms} Savivaldybė: { realEstate.Municipality} Mikrorajonas: { realEstate.Microdistrict} Gatvė: { realEstate.Street}"
+                Address = realEstate.Microdistrict + ", " + realEstate.Street,
+                PricePerSqM = realEstate.PricePerSqM,
+                NumberOfRooms = realEstate.NumberOfRooms,
+                BuildYear = realEstate.BuildYear,
+                Municipality = realEstate.Municipality,
+                Floor = realEstate.Floor,
+                Area = realEstate.Area,
+                Price = realEstate.Price,
+                Image = realEstate.Image,
+                Link = realEstate.Link
             };
-            pin.InfoWindowClicked += async (sender, e) =>
-            {
-                await Browser.OpenAsync(realEstate.Link, BrowserLaunchMode.SystemPreferred);
-            };
-            map.Pins.Add(pin);
-            container.Children.Add(map);
-        }
 
+            customMap.CustomPins = new List<CustomPin> { customPin };
+            customMap.Pins.Add(customPin);
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(realEstate.Latitude, realEstate.Longitude), Distance.FromMiles(1.0)));
+            container.Children.Add(customMap);
+        }
     }
 }
